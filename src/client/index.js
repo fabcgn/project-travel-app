0ee4274fb0b4b3063902edfc301f7cae6bcdec76
Fabian Seibt <faseibt@gmail.com>
2020-03-02 18:13:13 +0100

    extended basic setup

---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+/* 
+import { handleSubmit } from './js/formHandler'
+import { requestArticleInfo } from './js/articleInfo'
+
+console.log(checkForName);
+
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+import './styles/resultslist.scss'
+
+export {
+    handleSubmit,
+    requestArticleInfo
+} */
