9599db2edb4569d9ba281a6d886728587006c015
Fabian Seibt <faseibt@gmail.com>
2020-03-02 18:10:46 +0100

    basic setup of index.js

---
+++ src/server/index.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv')
+dotenv.config()
+var path = require('path')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(express.static('dist'))
+
+app.use(bodyParser.urlencoded({
+    extended: true
+})
+);
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+module.exports = app;
