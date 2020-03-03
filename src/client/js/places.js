47df12a8637b4714e74da2a528e835a0fa0a6a07
Fabian Seibt <faseibt@gmail.com>
2020-03-03 19:00:43 +0100

    places now returns only the first object of api re

--- src/client/js/places.js
+++ src/client/js/places.js
@@ -3,16 +3,19 @@ const geoNameUserName = "fabcgn"
 
 
 /* Function to get the current temperature by ZIP Code*/
-getLocation = async (city) => {
+getLatLng = async (city) => {
     const res = await fetch("http://api.geonames.org/postalCodeSearchJSON?placename=k%C3%B6ln&maxRows=10&username="+geoNameUserName)
     try {
         const place = await res.json();
-        console.log(place)
-        return place;
+        console.log(place.postalCodes[0])
+        console.log("Lat="+place.postalCodes[0].lat)
+        console.log("Lng="+place.postalCodes[0].lng)
+        console.log("CountryCode="+place.postalCodes[0].countryCode)
+        return place.postalCodes[0];
     } catch(error){
         console.log("ERROR",error)
     }
 }
 
-getLocation("Köln")
+getLatLng("Köln")
 
