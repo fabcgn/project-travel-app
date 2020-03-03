b7522c627eb93d51d99ebdd256ddbe0fd710d5b8
Fabian Seibt <faseibt@gmail.com>
2020-03-03 19:44:30 +0100

    Added fallback image for non found search

--- src/client/js/pictures.js
+++ src/client/js/pictures.js
@@ -1,27 +1,26 @@
 /* Global Variables */
 const pixabayApiKey = "15458697-86a1b8fb3157401dfeb55f13a"
-let featureImg = document.getElementById("cityPic").src
 
-/* Fucntion to exchange any image src by ID */
-const exchangeImg = (frame, image) => {
+/* Fucntion to exchange any image src by ID after delay */
+const exchangeImg = async (frame, image, delay) => {
     setTimeout(() => {
         document.getElementById(frame).src = image
-    }, 3000); // TODO: Delete Delay later
-    
+    }, delay); // TODO: Delete Delay later   
 }   
 
 /* Function to get the current temperature by ZIP Code*/
-const getImage = async (city) => {
-    const res = await fetch("https://pixabay.com/api/?key="+pixabayApiKey+"&q="+city+"&image_type=photo")
+const changeImageBySearchterm = async (frame, searchTerm) => {
+    const res = await fetch("https://pixabay.com/api/?key="+pixabayApiKey+"&q="+searchTerm+"&image_type=photo")
     try {
         const picture = await res.json();
         console.log(picture.hits[0].webformatURL)
         console.log(picture.hits[0])
-        return picture.hits[0];
+        exchangeImg (frame, picture.hits[0].webformatURL, 100)
+        return "success";
     } catch(error){
+        exchangeImg (frame, "/src/client/media/pictures/hiker-918473_640.jpg", 100)
         console.log("ERROR",error)
     }
 }
 
-getImage("New York")
-exchangeImg ("cityPic","/src/client/media/pictures/hiker-918473_640.jpg")
+changeImageBySearchterm("cityPic","jkdf,asm.fkfjdsnfgödfjksdnvcjk")
