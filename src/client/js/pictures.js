d861710b69ca34b34adcad978ee39e33c52c0214
Fabian Seibt <faseibt@gmail.com>
2020-03-03 19:56:15 +0100

    added alt of picture to be changed also

--- src/client/js/pictures.js
+++ src/client/js/pictures.js
@@ -2,9 +2,10 @@
 const pixabayApiKey = "15458697-86a1b8fb3157401dfeb55f13a"
 
 /* Fucntion to exchange any image src by ID after delay */
-const exchangeImg = async (frame, image, delay) => {
+const exchangeImg = async (frame, image, alt, delay) => {
     setTimeout(() => {
         document.getElementById(frame).src = image
+        document.getElementById(frame).alt = "Picture of "+alt
     }, delay); // TODO: Delete Delay later   
 }   
 
@@ -13,10 +14,9 @@ const changeImageBySearchterm = async (frame, searchTerm) => {
     const res = await fetch("https://pixabay.com/api/?key="+pixabayApiKey+"&q="+searchTerm+"&image_type=photo")
     try {
         const picture = await res.json();
-        console.log(picture.hits)
-        console.log(picture.hits.length)
         const randomEntry = Math.floor(Math.random() * picture.hits.length) // chooses a random picture to display
-        exchangeImg (frame, picture.hits[randomEntry].webformatURL, 100)
+        console.log(picture.hits[randomEntry])
+        exchangeImg (frame, picture.hits[randomEntry].webformatURL, searchTerm, 100)
         return "success";
     } catch(error){
         exchangeImg (frame, "/src/client/media/pictures/hiker-918473_640.jpg", 100)
