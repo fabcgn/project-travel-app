cde04f276e2a1b4439c6fa91dff4ccb93128c319
Fabian Seibt <faseibt@gmail.com>
2020-03-03 19:16:12 +0100

    getImage fucntion works great

--- src/client/js/pictures.js
+++ src/client/js/pictures.js
@@ -6,12 +6,13 @@ getImage = async (city) => {
     const res = await fetch("https://pixabay.com/api/?key="+pixabayApiKey+"&q="+city+"&image_type=photo")
     try {
         const picture = await res.json();
-        console.log(picture)
-        return picture;
+        console.log(picture.hits[0].webformatURL)
+        console.log(picture.hits[0])
+        return picture.hits[0];
     } catch(error){
         console.log("ERROR",error)
     }
 }
 
-getImage("Köln")
+getImage("New York")
 
