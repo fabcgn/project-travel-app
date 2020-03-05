77e8def8a77b336662f25041bd112bed38dd80f3
Fabian Seibt <faseibt@gmail.com>
2020-03-05 22:16:33 +0100

    added src logos

--- src/client/js/starter.js
+++ src/client/js/starter.js
@@ -13,12 +13,11 @@ export const listener = submitButton.addEventListener('click',function(event){
 
 const start = async (city) => {
     const latLng = await getLatLng(city)
-    console.log(latLng.lng) //TODO: Work with this thing to get the weather from server.js
-    changeImageBySearchterm("cityPic",city)
+    const date = getUnixTimeCode(document.getElementById("dateInput").value)
+    await getWeather({"lat": latLng.lat, "lng": latLng.lng, "time": date})
+    await changeImageBySearchterm("cityPic",city)
     document.getElementById("city").innerText = `City: ${city}`
     document.getElementById("lat").innerText = `Latitude: ${latLng.lat}`
     document.getElementById("lng").innerText = `Longitude: ${latLng.lng}`
     document.getElementById("country").innerText = `Country: ${latLng.countryCode}`
-    const date = getUnixTimeCode(document.getElementById("dateInput").value)
-    await getWeather({"lat": latLng.lat, "lng": latLng.lng, "time": date})
 }
