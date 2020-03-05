90cf4eab6be5237da12fa71909d6d4f11210d583
Fabian Seibt <faseibt@gmail.com>
2020-03-05 20:45:40 +0100

    date in weater is now populated from field

--- src/client/js/dateCalc.js
+++ src/client/js/dateCalc.js
@@ -3,4 +3,3 @@ export const getUnixTimeCode = (date) => {
     const UNIXdate = new Date(date).getTime() / 1000
     console.log(UNIXdate)
 }
-getUnixTimeCode("2020-03-28")
--- src/client/js/starter.js
+++ src/client/js/starter.js
@@ -1,6 +1,7 @@
 import { getLatLng } from './places'
 import { changeImageBySearchterm } from './pictures'
 import { getWeather } from './weather'
+import { getUnixTimeCode } from './dateCalc';
 
 const submitButton = document.getElementById("requestButton")
 export const listener = submitButton.addEventListener('click',function(event){
@@ -18,6 +19,6 @@ const start = async (city) => {
     document.getElementById("lat").innerText = `Latitude: ${latLng.lat}`
     document.getElementById("lng").innerText = `Longitude: ${latLng.lng}`
     document.getElementById("country").innerText = `Country: ${latLng.countryCode}`
-    console.log(document.getElementById("dateInput").value)
-    await getWeather({"lat": latLng.lat, "lng": latLng.lng, "time": 1583341111})
+    const date = getUnixTimeCode(document.getElementById("dateInput").value)
+    await getWeather({"lat": latLng.lat, "lng": latLng.lng, "time": date})
 }
