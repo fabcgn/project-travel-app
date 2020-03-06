abca50e534f1db5de833f191da0a04c7e82450db
Fabian Seibt <faseibt@gmail.com>
2020-03-06 20:03:30 +0100

    added Trip const to store trip data

--- src/client/js/pictures.js
+++ src/client/js/pictures.js
@@ -1,25 +1,18 @@
 /* Global Variables */
 const pixabayApiKey = "15458697-86a1b8fb3157401dfeb55f13a"
 
-/* Function to exchange any image src by ID after delay */
-const exchangeImg = (frame, image, alt, delay) => {
-    setTimeout(() => {
-        document.getElementById(frame).src = image
-        document.getElementById(frame).alt = "Picture of "+alt
-    }, delay);
-}   
-
 /* Function to get the current temperature by ZIP Code*/
-export const changeImageBySearchterm = async (frame, searchTerm) => {
+export const imageUrlBySearchterm = async (searchTerm) => {
     const res = await fetch("https://pixabay.com/api/?key="+pixabayApiKey+"&q="+searchTerm+"&image_type=photo")
     try {
         const picture = await res.json();
         const randomEntry = Math.floor(Math.random() * picture.hits.length) // chooses a random picture to display
         //console.log(picture.hits[randomEntry])
-        exchangeImg (frame, picture.hits[randomEntry].webformatURL, searchTerm, 0)
-        return "success";
+        const url = picture.hits[randomEntry].webformatURL
+        return url;
     } catch(error){
-        exchangeImg (frame, "/src/client/media/pictures/hiker-918473_640.jpg", "travels",0)
+        const url = "/src/client/media/pictures/hiker-918473_640.jpg"
         console.log("ERROR",error)
+        return url
     }
 }
--- src/client/js/starter.js
+++ src/client/js/starter.js
@@ -1,5 +1,5 @@
 import { getLatLng } from './places'
-import { changeImageBySearchterm } from './pictures'
+import { imageUrlBySearchterm } from './pictures'
 import { getWeather } from './weather'
 import { getUnixTimeCode } from './dateCalc'
 
@@ -12,16 +12,34 @@ export const listener = submitButton.addEventListener('click',function(event){
     start(city, time)
 })
 
+const trip = {}
+
 const start = async (city, time) => {
     const latLng = await getLatLng(city)
-    const UnixTime = await getUnixTimeCode(time)
-    const weather = await getWeather({"lat": latLng.lat, "lng": latLng.lng, "time": UnixTime})
-    console.log(weather)
-    await changeImageBySearchterm("cityPic",city)
+    trip.Latitude = latLng.lat
+    trip.Longitude = latLng.lng
+    trip.Country = latLng.countryCode
+    trip.unixtime = await getUnixTimeCode(time)
+    trip.img = await imageUrlBySearchterm(city)
+    const weather = await getWeather({"lat": latLng.lat, "lng": latLng.lng, "time": trip.unixtime})
+    trip.tempMax = weather.tempHigh
+    trip.tempMin = weather.tempLow
+    trip.weatherSummary = weather.Summary
     document.getElementById("city").innerText = `City: ${city}`
     document.getElementById("lat").innerText = `Latitude: ${latLng.lat}`
     document.getElementById("lng").innerText = `Longitude: ${latLng.lng}`
     document.getElementById("country").innerText = `Country: ${latLng.countryCode}`
     document.getElementById("weather").innerText = `Weather: ${JSON.stringify(weather)}`
+    console.log(trip)
 }
 
+start ("Cologne", "2020-03-09")
+
+/* To add later again: 
+export const exchangeImg = (frame, image, alt, delay) => {
+    setTimeout(() => {
+        document.getElementById(frame).src = image
+        document.getElementById(frame).alt = "Picture of "+alt
+    }, delay);
+}   
+*/
