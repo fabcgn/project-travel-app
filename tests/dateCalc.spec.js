188af21d10ca392a11fbef38ffacee2a49f0eb32
Fabian Seibt <faseibt@gmail.com>
2020-03-07 11:14:18 +0100

    multiple testcases

--- tests/dateCalc.spec.js
+++ tests/dateCalc.spec.js
@@ -2,10 +2,19 @@ import { getUnixTimeCode } from '../src/client/js/dateCalc'
 
 
 describe("Date Calculation Function", () => {
-    test("Proper UNIX timecode is calculated", () => {
+    test("Proper UNIX timecode is calculated #1", () => {
         const input = "2020-03-01"
         const output = 1583020800
-
         expect(getUnixTimeCode(input)).toEqual(output);
     })
-});
+    test("Proper UNIX timecode is calculated #2", () => {
+        const input = "1991-05-01"
+        const output = 673056000
+        expect(getUnixTimeCode(input)).toEqual(output);
+    })
+    test("Proper UNIX timecode is calculated from year", () => {
+        const input = "2020"
+        const output = 1577836800
+        expect(getUnixTimeCode(input)).toEqual(output);
+    })
+})
